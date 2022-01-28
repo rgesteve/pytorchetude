b52cd75687142b07365ef9691703a0c0fbab58b8
R. G. Esteves <rgesteves@gmail.com>
2022-01-28 12:50:50 +0000

    Beginning of frontend script

---
+++ bin/augment_dataset.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../ml_prepare')
+sys.path.append('../deep_tabular_augmentation')
+
+import deep_tabular_augmentation as dta
+
+if __name__ == '__main__':
+    print("Hello, world!")
